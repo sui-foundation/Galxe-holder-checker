9b2f21f722e8775ecfd3908db9efa2f98690474f
Antonio Yang <yanganto@gmail.com>
2025-04-09 12:18:35 +0800

    pass coin addrees in service

--- nix/service.nix
+++ nix/service.nix
@@ -10,6 +10,11 @@ in
       type = types.package;
       description = "The Galxe holder checker service binary";
     };
+    coinAddrs= mkOption {
+      type = lib.types.listOf types.str;
+      default = [ "usdc=0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf" ];
+      description = "The package address for coin";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -20,7 +25,7 @@ in
     unitConfig.X-StopOnRemoval = false;
     path = [ cfg.package ];
     script = ''
-      ${cfg.package}/bin/ghc
+      ${cfg.package}/bin/ghc ${lib.strings.concatImapStrings (o: " -c " + o) cfg.coinAddrs}
     '';
     serviceConfig = {
       WorkingDirectory = "~";
